a9ffaf1d22b227fc63eeb5f0b856dd92e93ec251
leivanoe1011 <leiva.noe.86@gmail.com>
2016-10-17 13:20:37 -0700

    Update DataToDo.cs

--- ToDoMVC/ToDoMVC.Domain/DataToDo.cs
+++ ToDoMVC/ToDoMVC.Domain/DataToDo.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace ToDoMVC.Domain
 {
+
+    /// <summary>
+    /// Responsibilities:
+    /// DTO for DBSet User object.
+    /// </summary>
+    
     public class DataToDo
     {
         public string Name { get; set; }
