6dfe5ced4b9e0ac9f4551d8bb9e56b43cd444921
leivanoe1011 <leiva.noe.86@gmail.com>
2016-10-17 13:21:40 -0700

    Update DataToDo.cs

--- ToDoMVC/ToDoMVC.Domain/DataToDo.cs
+++ ToDoMVC/ToDoMVC.Domain/DataToDo.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace ToDoMVC.Domain
 {
+
+    /// <summary>
+    /// Responsibilities:
+    /// Get and Set Methods for both Name and UserName strings
+    /// </summary>
+    
     public class DataToDo
     {
         public string Name { get; set; }
