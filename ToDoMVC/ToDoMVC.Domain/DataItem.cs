39bf4f42bc9c19a20dd3e8559fb6e56788f5fc3c
leivanoe1011 <leiva.noe.86@gmail.com>
2016-10-19 22:12:30 -0700

    Update DataItem.cs

--- ToDoMVC/ToDoMVC.Domain/DataItem.cs
+++ ToDoMVC/ToDoMVC.Domain/DataItem.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    /// <summary>
+    /// Responsibilities:
+    /// The Class will create the object that will
+    /// make the entries in the DB.
+    /// </summary>
 namespace ToDoMVC.Domain
 {
     public class DataItem
     {
+        
+        /// Below are the different columns that will be 
+        /// stored in the DB.
         public string Name { get; set; }
         public int ToDoId { get; set; }
         public int Id { get; set; }
