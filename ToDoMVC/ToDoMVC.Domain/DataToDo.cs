a669bee0299c57408dce8e28a93a037f8d0696d1
leivanoe1011 <leiva.noe.86@gmail.com>
2016-10-19 22:11:38 -0700

    Update DataToDo.cs

--- ToDoMVC/ToDoMVC.Domain/DataToDo.cs
+++ ToDoMVC/ToDoMVC.Domain/DataToDo.cs
@@ -4,8 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+    /// <summary>
+    /// Responsibilities:
+    /// The Class will create the object that will
+    /// make the entries in the DB.
+    /// </summary>
+
 namespace ToDoMVC.Domain
 {
+    /// Below are the different columns that will be 
+    /// stored in the DB.
     public class DataToDo
     {
         public string Name { get; set; }
